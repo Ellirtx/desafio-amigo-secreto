00b03b4a3dcca514fd780e99f280a0938ad253a3
Elivelton de Lima <elivelton.oliveira776@gmail.com>
2025-09-20 15:37:00 -0300

    Adiciona verificação de nomes duplicados e refatoração

--- app.js
+++ app.js
@@ -5,29 +5,31 @@ function limparCampo() {
     campo.value = '';
 }
 
-function adicionarAmigo() {
-    let nomeInse = document.getElementById('amigo').value.trim(); 
-    let msgErro = document.getElementById('mensagemDerro');
+function agregarAmigo() {
+    const nomeInse = document.getElementById('amigo').value.trim(); 
+    const msgErro = document.getElementById('mensagemDerro');
 
-    if (nomeInse === '') {
-        msgErro.innerHTML = 'Campo vazio! Insira um nome !!';
+    if (nomeInse === '' || amigos.includes(nomeInse)) {
+        msgErro.textContent = nomeInse === ''
+        ? 'Campo vazio ! Insira um nome !!' : 'Esse nome já existe na lista!!';
         limparCampo();
-    } else {
+    } else { 
         amigos.push(nomeInse);
         msgErro.innerText = '';
-        adicionarAmigoarray();
+        adicionarAmigosLista();
         limparCampo();
     }
 }
 
-function adicionarAmigoarray() {
+function adicionarAmigosLista() {
     const lista = document.getElementById("listaAmigos");
 
-    lista.innerHTML = "";
+    lista.textContent = "";
 
     for (let i = 0; i < amigos.length; i++) {
     const li = document.createElement("li");
     li.textContent = amigos[i];
+     
     listaAmigos.appendChild(li);
   }
 }
